d3746809879a64cc2de7649ab78e5a0f49edb70d
lgehring <lukasgehring@Lukass-MacBook-Pro.local>
2021-05-26 12:49:17 +0200

    Create table and fill with keyfield values

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import configparser
 
-import mysql.connector
+from mysql.connector import connect, Error, ProgrammingError
 
 
 def load_config():
@@ -29,39 +29,40 @@ def get_mysql_connection_and_table_name(config):
     password = database_config['password']
     table_name = database_config['table']
 
-    mysql_connection = mysql.connector.connect(
-        host=host,
-        user=user,
-        database=database,
-        password=password
-    )
+    try:
+        connection = connect(
+            host=host,
+            user=user,
+            database=database,
+            password=password
+        )
 
-    create_table_if_not_exists(mysql_connection, table_name, config['EXPERIMENT'])
+        create_table_if_not_exists(connection, table_name, config['EXPERIMENT'])
 
-    return mysql_connection, table_name
+        return connection, table_name
+    except Error as e:
+        print(e)
 
 
-def create_table_if_not_exists(mysql_connection, table_name, experiment_config) -> None:
+def create_table_if_not_exists(connection, table_name, experiment_config) -> None:
     """
     Check if tables does exist. If not, a new table will be created.
     :param mysql_connection: mysql_connector to the database
     :param table_name: name of the table from the config
     :param experiment_config: experiment section of the config file
     """
-    cursor = mysql_connection.cursor()
-    cursor.execute('SHOW TABLES')
 
+    cursor = connection.cursor(buffered=True)
+    cursor.execute('SHOW TABLES')
     for table in cursor:
         if table[0] == table_name:
-            print('table already exists in database')
             return
 
     query = create_new_table_query(table_name, experiment_config)
 
     try:
         cursor.execute(query)
-        print('table "%s" created' % table_name)
-    except mysql.connector.ProgrammingError as err:
+    except ProgrammingError as err:
         unkown_datatype = str(err.__context__).split("'")[1].split(" ")[0]
         print("Error: '%s' is unknown or not allowed" % unkown_datatype)
 
@@ -79,8 +80,10 @@ def create_new_table_query(table_name: str, experiment_config) -> str:
     typed_fields = [tuple(field.split(':')) if len(field.split(':')) == 2 else (field, 'VARCHAR(255)') for
                        field in clean_fields]
 
+    typed_fields.extend([('status', 'VARCHAR(255)'), ('creation_date', 'VARCHAR(255)'), ('start_data', 'VARCHAR(255)'), ('end_data', 'VARCHAR(255)')])
+
     for field, datatype in typed_fields:
-        query += '%s %s NOT NULL, ' % (field, datatype)
+        query += '%s %s DEFAULT NULL, ' % (field, datatype)
 
     # remove last ', '
     query = query[:-2] + ')'
