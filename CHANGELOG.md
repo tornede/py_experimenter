6bd8676ec4cab15e45b1df342e593b1f76e7021a
tornede <tornede@mail.uni-paderborn.de>
2022-08-08 16:50:53 +0200

    Update setup.py, licence, changelog and authors

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+
+
+## v1.0.0 (08/08/2022)
+
+### Feature
+
+- Added multiple options to fill tables
+- Improved robustness of database creation and experiment execution
+- Improved error handling
+
+### Fix
+
+- Checked when re-filling a table, that only missing rows are added and no duplicated rows are created
+
+### Tests
+
+- Added tests for all key components of `PyExperimenter`
+
+## v0.0.1 (14/10/2021)
+
+- First release of `PyExperimenter`
--- base_folder/setup.py
+++ base_folder/setup.py
@@ -9,16 +9,18 @@ VERSION = '0.0.4'
 setup(
     name="py_experimenter",
     version=VERSION,
-    author="Lukas Gehring",
-    author_email="lgehring@mail.upb.de",
+    author="Tanja Tornede, Alexander Tornede",
+    author_email="tanja.tornede@upb.de, alexander.tornede@upb.de",
     description="""The PyExperimenter is a tool for the automatic execution of various experiments.""",
     long_description_content_type="text/markdown",
     long_description="""
-    With the PyExperimenter user can create a function that run one single experiment (we call this function `own_function`, but any function name is possible). The experiment itself,
-    which is defined by its parameters, is then automatically executed by passing the parameters to the `own_function` and writing the
-    results in a database. Errors that occur during execution are also captured by the PyExperimenter and written to the database.
-    For the complete documentation, please visit the GitHub: https://github.com/lukasgehring/py_experimenter""",
+        The PyExperimenter is a tool for the automatic execution of various experiments. The user can create a function 
+        to run a single experiment (we call this function `own_function`, but any function name is possible). The experiment itself, 
+        which is defined by its parameters, is then automatically executed by passing the parameters to the `own_function` and writing the
+        results into a database. Errors that occur during execution are also captured by the PyExperimenter and written to the database.
+        For the complete documentation, please visit GitHub: https://github.com/alexandertornede/py_experimenter
+    """,
     packages=find_packages(),
-    install_requires=['mysql-connector-python', 'numpy', 'pandas'],
-    keywords=['python', 'executor']
+    install_requires=['numpy', 'pandas', 'mysql-connector-python'],
+    keywords=['python', 'experiments', 'executor']
 )
